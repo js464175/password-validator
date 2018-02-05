079f98ff8938a1843ed73c9352fdba9a42844802
js464175 <js464175@dal.ca>
2018-02-04 22:26:44 -0400

    Initial commit

---
+++ app/src/main/java/com/dal/js464175/password_validator/Validator.java
@@ -0,0 +1,13 @@
+package com.dal.js464175.password_validator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Validator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validator);
+    }
+}
